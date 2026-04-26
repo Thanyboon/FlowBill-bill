0cb3338e589c5761bfb546de9a9980429e9d1f67
Thanyboon <140832807+Thanyboon@users.noreply.github.com>
2026-04-26 22:09:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,28 @@
-const cacheName = 'flowbill-v10'; // เปลี่ยนเลขเวอร์ชั่นตรงนี้
+const cacheName = 'flowbill-v11'; // เปลี่ยนเลข v ทุกครั้งที่มีการอัปเดตหน้าเว็บ
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim()); // บังคับให้เริ่มทำงานทันที
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
