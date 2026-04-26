647514df7e7de8e3b456e0cf78f436e3204d2449
Thanyboon <140832807+Thanyboon@users.noreply.github.com>
2026-04-26 21:29:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,22 @@
-const cacheName = 'flowbill-v1';
-const staticAssets = ['./index.html'];
+const cacheName = 'flowbill-v7'; // เปลี่ยนเลขเวอร์ชั่นเวลาอัปเดตแอป
+const staticAssets = [
+  './',
+  './index.html'
+];
 
-self.addEventListener('install', async (e) => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+// ติดตั้ง Service Worker และเก็บไฟล์ลง Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(staticAssets))
+  );
+  self.skipWaiting();
 });
 
+// กลยุทธ์: Network First (ดึงเน็ตก่อน ถ้าล้มเหลวค่อยใช้ Cache)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
